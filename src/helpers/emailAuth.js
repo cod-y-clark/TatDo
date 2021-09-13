278cd5d4f3b4735607e0470d00621cd6250d315b
cod-y-clark <cody66clark@gmail.com>
2021-09-13 18:46:57 -0500

    setting up capstone repo

---
+++ src/helpers/emailAuth.js
@@ -0,0 +1,69 @@
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+  } from "firebase/auth";
+  import { api } from "../api/dataAccess";
+  
+  // Notes!
+  // emailAuth.register() -- 1 param -- (full userObject)
+  // emailAuth.signIn() -- 1 param -- (full userObject)
+  // emailAuth.signOut() -- 0 param
+  
+  // userObject expected ---->
+  // {
+  //   email: "",
+  //   password: "",
+  //   displayName: "",
+  // }
+  
+  export const emailAuth = {
+    // Register New User
+    register: function (userObj) {
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, userObj.email, userObj.password)
+        .then((userCredential) => {
+          // change this if you want different user information
+          const userAuth = {
+            email: userCredential.user.email,
+            displayName: userObj.displayName,
+            uid: userCredential.user.uid,
+          };
+          // This adds the new user to your database
+          api.postRequest("users", userAuth);
+        })
+        .catch((error) => {
+          console.log("error code", error.code);
+          console.log("error message", error.message);
+        });
+    },
+    // Sign in existing user
+    signIn: function (userObj) {
+      return new Promise((res) => {
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, userObj.email, userObj.password)
+          .then((userCredential) => {
+            // returns user if you need it
+            const user = userCredential.user;
+            res(user);
+          })
+          .catch((error) => {
+            console.log("error code", error.code);
+            console.log("error message", error.message);
+          });
+      });
+    },
+    // Sign out
+    signOut: function () {
+      const auth = getAuth();
+      signOut(auth)
+        .then(() => {
+          console.log("Sign Out Success!");
+        })
+        .catch((error) => {
+          console.log("error code", error.code);
+          console.log("error message", error.message);
+        });
+    },
+  };
---
+++ src/views/signInEmail.js
@@ -0,0 +1,127 @@
+import react, { useState } from "react";
+import { emailAuth } from "../helpers/emailAuth"; // !! Make sure to import your functions
+import { Button, Form, Header, Grid } from "semantic-ui-react";
+
+function SignInEmail() {
+  const [register, setRegister] = useState(false);
+  // Set your user object
+  const [userObject, setUserObject] = useState({
+    email: "",
+    password: "",
+    displayName: "",
+  });
+
+  const onFieldChange = (event) => {
+    const newUser = { ...userObject };
+    newUser[event.target.id] = event.target.value;
+    setUserObject(newUser);
+  };
+
+  // Here! Here! Here's your signIn!
+  const onSubmitLogin = () => {
+    emailAuth.signIn(userObject);
+  };
+
+  // Here! Here! Here's your Register!
+  const onSubmitRegister = () => {
+    emailAuth.register(userObject);
+  };
+
+  // Don't forget you can also use the emailAuth.signOut() function
+
+  return (
+    <>
+      <Grid>
+        {!register && (
+          <Grid.Row centered columns={3}>
+            <Grid.Column>
+              <Header as="h2" textAlign="center">
+                Login
+              </Header>
+              <Form>
+                <Form.Field></Form.Field>
+                <Form.Field>
+                  <label>Email</label>
+                  <input
+                    id="email"
+                    onChange={onFieldChange}
+                    placeholder="example@email.com"
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>Password</label>
+                  <input
+                    id="password"
+                    onChange={onFieldChange}
+                    placeholder="1234@@"
+                  />
+                </Form.Field>
+                <Button type="submit" color="teal" onClick={onSubmitLogin}>
+                  Submit
+                </Button>
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
+        )}
+        {register && (
+          <Grid.Row centered columns={3}>
+            <Grid.Column>
+              <Header as="h2" textAlign="center">
+                Register
+              </Header>
+              <Form>
+                <Form.Field>
+                  <label>Name</label>
+                  <input
+                    id="displayName"
+                    onChange={onFieldChange}
+                    placeholder="Jane Doe"
+                  />
+                  <label>Email</label>
+                  <input
+                    id="email"
+                    onChange={onFieldChange}
+                    placeholder="example@email.com"
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>Password</label>
+                  <input
+                    id="password"
+                    onChange={onFieldChange}
+                    placeholder="1234@@"
+                  />
+                </Form.Field>
+                <Button type="submit" color="teal" onClick={onSubmitRegister}>
+                  Submit
+                </Button>
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
+        )}
+        <Grid.Row centered columns={3}>
+          <Grid.Column>
+            <Button
+              basic
+              color="green"
+              disabled={!register}
+              onClick={() => setRegister(false)}
+            >
+              Login
+            </Button>
+            <Button
+              basic
+              color="green"
+              disabled={register}
+              onClick={() => setRegister(true)}
+            >
+              Register
+            </Button>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </>
+  );
+}
+
+export default SignInEmail;
